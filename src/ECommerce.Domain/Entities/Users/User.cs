a323ec2d222440056479c0f35f49d5537492141e
shakhriyor akhadov <shakhriyor.akhadov@gmail.com>
2023-02-22 23:59:31 +0500

    some changes

---
+++ src/ECommerce.Domain/Entities/Users/User.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Domain.Commons;
+using ECommerce.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.Users
+{
+    public class User : Auditable
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string ImagePath { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; } = String.Empty;
+        public string Salt { get; set; } = String.Empty;
+        public DateTime UpdatedAt { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
